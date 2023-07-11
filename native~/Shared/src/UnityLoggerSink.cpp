db3882794d291b2d43c41d7c749a8ba317c5b79c
dy <dymail02@gmail.com>
2023-07-11 09:43:27 +0800

    屏蔽warn

--- native~/Shared/src/UnityLoggerSink.cpp
+++ native~/Shared/src/UnityLoggerSink.cpp
@@ -8,9 +8,16 @@ using namespace CesiumForUnity;
 using namespace DotNet;
 
 void UnityLoggerSink::sink_it_(const spdlog::details::log_msg& msg) {
-  spdlog::memory_buf_t formatted;
-  this->formatter_->format(msg, formatted);
-  UnityEngine::Debug::Log(System::String(fmt::to_string(formatted)));
+  switch (msg.level) {
+  case spdlog::level::warn:
+    break;
+  default:
+    spdlog::memory_buf_t formatted;
+    this->formatter_->format(msg, formatted);
+    UnityEngine::Debug::Log(System::String(fmt::to_string(formatted)));
+    break;
+  }
+
 }
 
 void UnityLoggerSink::flush_() {}
